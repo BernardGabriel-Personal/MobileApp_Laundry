31691906a4a4b88fb6cee4149ab8371cc99b4b37
BernardGabriel-Personal <bernardgabriel151@gmail.com>
2025-04-27 16:57:13 +0800

    Fixed customer signup keyboard overlapping

--- lib/Start,Signup,Login/6_customer_signup.dart
+++ lib/Start,Signup,Login/6_customer_signup.dart
@@ -110,7 +110,13 @@ class _CustomerSignUpScreenState extends State<CustomerSignUpScreen> {
     return RegExp(r'^\d{11}$').hasMatch(number);
   }
 
-  void _signUp() {
+  bool _isLoading = false;
+
+  void _signUp() async {
+    setState(() {
+      _isLoading = true; // Start the loading spinner
+    });
+
     String fullName = _fullNameController.text.trim();
     String email = _emailController.text.trim();
     String contact = _contactController.text.trim();
@@ -118,143 +124,183 @@ class _CustomerSignUpScreenState extends State<CustomerSignUpScreen> {
     String password = _passwordController.text;
 
     if (fullName.isEmpty || email.isEmpty || contact.isEmpty || address.isEmpty || password.isEmpty) {
+      setState(() {
+        _isLoading = false; // Stop the loading spinner
+      });
       _showErrorDialog("Please fill in all fields.");
       return;
     }
 
     if (!_isValidEmail(email)) {
+      setState(() {
+        _isLoading = false; // Stop the loading spinner
+      });
       _showErrorDialog("Please enter a valid email address.");
       return;
     }
 
     if (!_isValidContactNumber(contact)) {
+      setState(() {
+        _isLoading = false; // Stop the loading spinner
+      });
       _showErrorDialog("Please enter a valid 11-digit contact number.");
       return;
     }
 
     if (password.length < 6) {
+      setState(() {
+        _isLoading = false; // Stop the loading spinner
+      });
       _showErrorDialog("Password must be at least 6 characters long.");
       return;
     }
 
+    // Simulate a delay for signing up (this is where your firebase logic would go)
+    await Future.delayed(const Duration(seconds: 2));
+
+    setState(() {
+      _isLoading = false; // Stop the loading spinner
+    });
+
     _showSuccessDialog("Customer signed up successfully!");
-    // firebase logic here
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.greenAccent, Colors.white],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 80),
-                  const SizedBox(height: 115),
-                  const Text(
-                    'Customer Sign-Up',
-                    style: TextStyle(
-                      fontFamily: 'IndieFlower',
-                      shadows: [
-                        Shadow(
-                          offset: Offset(4.0, 6.0),
-                          blurRadius: 10.0,
-                          color: Colors.grey,
-                        ),
-                      ],
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold,
-                    ),
+      resizeToAvoidBottomInset: true, // Allow the screen to resize when keyboard shows up
+      body: Scrollbar( // Wrap the SingleChildScrollView with Scrollbar
+        child: SingleChildScrollView( // Wrap the entire body in SingleChildScrollView
+          child: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.greenAccent, Colors.white],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
                   ),
-                  const SizedBox(height: 50),
-
-                  _buildTextField(label: 'Full Name', controller: _fullNameController),
-                  const SizedBox(height: 16),
-
-                  _buildTextField(
-                    label: 'Email Address',
-                    controller: _emailController,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(height: 16),
-
-                  _buildTextField(
-                    label: 'Contact Number',
-                    controller: _contactController,
-                    keyboardType: TextInputType.phone,
-                  ),
-                  const SizedBox(height: 16),
-
-                  _buildTextField(
-                    label: 'Current Home Address',
-                    controller: _addressController,
-                  ),
-                  const SizedBox(height: 16),
-
-                  _buildTextField(
-                    label: 'Password',
-                    controller: _passwordController,
-                    isPassword: true,
-                  ),
-                  const SizedBox(height: 30),
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: _signUp,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF04D26F),
-                        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 80),
+                      const SizedBox(height: 115),
+                      const Text(
+                        'Customer Sign-Up',
+                        style: TextStyle(
+                          fontFamily: 'IndieFlower',
+                          shadows: [
+                            Shadow(
+                              offset: Offset(4.0, 6.0),
+                              blurRadius: 10.0,
+                              color: Colors.grey,
+                            ),
+                          ],
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      const SizedBox(height: 50),
+
+                      // Full Name Input
+                      _buildTextField(label: 'Full Name', controller: _fullNameController),
+                      const SizedBox(height: 16),
+
+                      // Email Address Input
+                      _buildTextField(
+                        label: 'Email Address',
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
                       ),
-                    ),
+                      const SizedBox(height: 16),
+
+                      // Contact Number Input
+                      _buildTextField(
+                        label: 'Contact Number',
+                        controller: _contactController,
+                        keyboardType: TextInputType.phone,
+                      ),
+                      const SizedBox(height: 16),
+
+                      // Current Home Address Input
+                      _buildTextField(
+                        label: 'Current Home Address',
+                        controller: _addressController,
+                      ),
+                      const SizedBox(height: 16),
+
+                      // Password Input
+                      _buildTextField(
+                        label: 'Password',
+                        controller: _passwordController,
+                        isPassword: true,
+                      ),
+                      const SizedBox(height: 30),
+
+                      // Sign Up Button
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: _isLoading ? null : _signUp,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF04D26F),
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 32,
+                              vertical: 12,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: _isLoading
+                              ? const CircularProgressIndicator(
+                            color: Color(0xFF04D26F),
+                          )
+                              : const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-
-          Positioned(
-            top: -20,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Image.asset(
-                'assets/FiveStarsLaundromat.png',
-                height: 250,
+              Positioned(
+                top: -20,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Image.asset(
+                    'assets/FiveStarsLaundromat.png',
+                    height: 250,
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          Positioned(
-            top: MediaQuery.of(context).padding.top + 15,
-            left: 15,
-            child: IconButton(
-              icon: const Icon(Icons.arrow_back_ios),
-              color: Colors.grey[500],
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+              // Positioned Back Button (top-left corner)
+              Positioned(
+                top: MediaQuery.of(context).padding.top + 15, // Add padding for safe area
+                left: 15,
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios),
+                  color: Colors.grey[500],
+                  onPressed: () {
+                    Navigator.pop(context); // Navigate back to the previous screen
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
